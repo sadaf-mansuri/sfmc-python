f76d567ca86ceddb16478f08c3370783c16aa6cc
smansuri <smansuri@live.com>
2024-10-16 10:46:23 -0400

    added

--- getAllTemplates.py
+++ getAllTemplates.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import os
 
 # Step 1: Get OAuth token
 auth_url = 'https://YOUR_SUBDOMAIN.auth.marketingcloudapis.com/v2/token'
@@ -26,5 +28,13 @@ params = {
 response = requests.get(api_url, headers=headers, params=params)
 email_templates = response.json()
 
-# Output email templates data
-print(email_templates)
+# Step 3: Parse the JSON and save HTML templates to files
+for item in email_templates['items']:
+    file_name = item['fileProperties']['fileName'] if 'fileProperties' in item else f'template_{item["id"]}.html'
+    html_content = item['views']['html']['content'] if 'views' in item and 'html' in item['views'] else ''
+    
+    # Save the HTML content to a file
+    with open(file_name, 'w', encoding='utf-8') as file:
+        file.write(html_content)
+
+    print(f"Saved template {item['name']} as {file_name}")
